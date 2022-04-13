d764e47ef835977d51f4d100511493be0959ab4b
h.hayati <hoseinhayati.ir@gmail.com>
2022-04-13 11:13:33 +0430

    fix-biz

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Asp.netCore_MVC_.Biz;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Asp.netCore_MVC_.Data;
 using Asp.netCore_MVC_.Models;
-using Asp.netCore_MVC_.Repo;
 using Asp.netCore_MVC_.ViewModel;
 using AutoMapper;
 
